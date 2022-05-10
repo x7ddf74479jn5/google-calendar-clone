38fa483509d2da1ee9a9a2899bb2aa257fa966b6
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-05-10 23:43:22 +0900

    🚧 wip: context

---
+++ src/context/GlobalContext.tsx
@@ -0,0 +1,24 @@
+import { createContext } from "react";
+
+const GlobalContext = createContext({
+  monthIndex: 0,
+  setMonthIndex: (index) => {},
+  smallCalendarMonth: 0,
+  setSmallCalendarMonth: (index) => {},
+  daySelected: null,
+  setDaySelected: (day) => {},
+  showEventModal: false,
+  setShowEventModal: () => {},
+  dispatchCalEvent: ({ type, payload }) => {},
+  savedEvents: [],
+  selectedEvent: null,
+  setSelectedEvent: () => {},
+  setLabels: () => {},
+  labels: [],
+  updateLabel: () => {},
+  filteredEvents: [],
+  loadingEvents: true,
+  setLoadingEvents: () => {},
+});
+
+export default GlobalContext;
